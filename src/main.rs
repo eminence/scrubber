a44366cd1323268372479b37664d6bcc37c9468f
Andrew Chin <achin@eminence32.net>
2017-11-10 23:34:25 -0500

    Misc updates and reformatting

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 extern crate time;
 
-use std::env::{home_dir};
+use std::env::home_dir;
 use std::path::{Path, PathBuf};
 use std::fs::{read_dir, remove_file, remove_dir_all};
 use std::os::unix::fs::MetadataExt;
@@ -43,11 +43,14 @@ fn can_be_removed<P: AsRef<Path>>(dir: P) -> bool {
         if entry.is_dir() {
             remove = remove && can_be_removed(entry);
         } else {
-            if !file_is_old(entry) {
+            if !file_is_old(&entry) {
                 remove = false;
+                println!("{:?} is not old", entry);
             }
         }
-        if !remove { break; }
+        if !remove {
+            break;
+        }
     }
 
     remove
@@ -56,25 +59,38 @@ fn can_be_removed<P: AsRef<Path>>(dir: P) -> bool {
 fn remove<P: AsRef<Path>>(path: P) {
     let path = path.as_ref();
     if path.is_dir() {
-        remove_dir_all(path);
+        remove_dir_all(path).unwrap();
     } else {
-        remove_file(path);
+        remove_file(path).unwrap();
     }
-    
+
 }
 
 fn main() {
     let mut mytmp: PathBuf = home_dir().expect("Unable to determine HOME directory");
     mytmp.push("tmp");
 
+    let ok_to_remove = std::env::args().nth(1).map_or(false, |x| x == "-go");
 
     for entry in read_dir(&mytmp).unwrap_or_else(|_| panic!("Unable to read_dir: {:?}", mytmp)) {
         if let Ok(entry) = entry {
             let entry_path = entry.path();
 
+            // only consider directories that seem to be a 2-digit number
+            let file_name = entry.file_name();
+            let name = file_name.to_string_lossy();
+            if !(name.len() == 2 &&
+                 name.char_indices().all(|(idx, chr)| idx < 2 && chr.is_digit(10))) {
+                println!("Will not examine {}", entry_path.display());
+                continue;
+            }
+
+
             if can_be_removed(&entry_path) {
-                //println!("{} can be removed", entry_path.display());
-                remove(&entry_path);
+                println!("{} can be removed", entry_path.display());
+                if ok_to_remove {
+                    remove(&entry_path);
+                }
             } else {
                 println!("must save {}", entry_path.display());
             }
