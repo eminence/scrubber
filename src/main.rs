639413ed06d4bccaf7ca676a8d60123e24481bf0
Andrew Chin <achin@eminence32.net>
2018-04-03 10:38:24 -0400

    Stuff
    Make crossplatform

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-//#![feature(fs_time)]
-
 extern crate time;
+extern crate clap;
 
 use std::env::home_dir;
 use std::path::{Path, PathBuf};
 use std::fs::{read_dir, remove_file, remove_dir_all};
-use std::os::unix::fs::MetadataExt;
-use time::{Duration, now_utc, at_utc, Timespec};
+use std::time::{SystemTime, Duration};
 
 fn file_is_old<P: AsRef<Path>>(f: P) -> bool {
     let f: &Path = f.as_ref();
-    let old = Duration::weeks(3);
-    let now = now_utc();
+    let old = Duration::from_secs(60 * 60 * 24 * 21);
+    let now = SystemTime::now();
     if let Ok(md) = f.metadata() {
-        let mda = at_utc(Timespec::new(md.atime() as i64, 0));
-        let mdm = at_utc(Timespec::new(md.mtime() as i64, 0));
+        let mda = md.accessed().ok();
+        let mdm = md.modified().ok();
 
-        if (now - mda < old) || (now - mdm < old) {
+        let keep_due_to_mtime: bool = mdm.map_or(true, |t| now.duration_since(t).map(|t| t < old).unwrap_or(true));
+        let keep_due_to_atime: bool = mda.map_or(true, |t| now.duration_since(t).map(|t| t < old).unwrap_or(true));
+
+
+        if keep_due_to_mtime || keep_due_to_atime {
             false
         } else {
             //println!("{:?} is old", f);
@@ -28,27 +30,52 @@ fn file_is_old<P: AsRef<Path>>(f: P) -> bool {
     }
 }
 
+enum Removable {
+    True,
+    /// If this path can't be removed, include why
+    False(PathBuf)
+}
 
-fn can_be_removed<P: AsRef<Path>>(dir: P) -> bool {
+impl Removable {
+    fn as_bool(&self) -> bool {
+        match *self {
+            Removable::True => true,
+            Removable::False(_) => false,
+        }
+    }
+    fn and(&mut self, other: Removable) {
+        if let Removable::False(_) = *self {
+            return;
+        }
+        if let Removable::False(thing) = other {
+            *self = Removable::False(thing);
+        }
+    }
+}
+
+fn can_be_removed<P: AsRef<Path>>(dir: P) -> Removable {
     let dir = dir.as_ref();
 
     if dir.is_file() {
-        return file_is_old(dir);
+        return match file_is_old(dir) {
+            True => Removable::True,
+            False => Removable::False(dir.to_owned())
+        }
     } // else is_dir
 
 
-    let mut remove = true;
+    let mut remove = Removable::True;
     for entry in read_dir(dir).unwrap() {
         let entry = entry.unwrap().path();
         if entry.is_dir() {
-            remove = remove && can_be_removed(entry);
+            remove.and(can_be_removed(entry));
         } else {
             if !file_is_old(&entry) {
-                remove = false;
-                println!("{:?} is not old", entry);
+                remove = Removable::False(entry.to_owned());
+//                println!("{:?} has been modified recently", entry);
             }
         }
-        if !remove {
+        if !remove.as_bool() {
             break;
         }
     }
@@ -67,10 +94,38 @@ fn remove<P: AsRef<Path>>(path: P) {
 }
 
 fn main() {
-    let mut mytmp: PathBuf = home_dir().expect("Unable to determine HOME directory");
-    mytmp.push("tmp");
+    use clap::{App, Arg};
 
-    let ok_to_remove = std::env::args().nth(1).map_or(false, |x| x == "-go");
+    let matches =
+        App::new("scrubber")
+        .version("0.0.1")
+        .about("Removes unused folders from a temp directory")
+        .arg(Arg::with_name("rm")
+             .long("rm")
+             .help("Actually remove directories"))
+        .arg(Arg::with_name("tmpdir")
+             .index(1)
+             .required(false)
+             .help("Path to tempdir.  Defaults to $HOME/tmp"))
+        .get_matches();
+
+
+    let mytmp: PathBuf = if let Some(t) = matches.value_of("tmpdir") {
+        let p = PathBuf::from(t);
+        if !p.exists() {
+            eprintln!("Tmpdir {} does not exist!", p.display());
+            std::process::exit(1)
+        }
+        p
+    } else {
+
+        let mut mytmp: PathBuf = home_dir().expect("Unable to determine HOME directory");
+        mytmp.push("tmp");
+        mytmp
+    };
+
+
+    let ok_to_remove = matches.is_present("rm");
 
     for entry in read_dir(&mytmp).unwrap_or_else(|_| panic!("Unable to read_dir: {:?}", mytmp)) {
         if let Ok(entry) = entry {
@@ -86,13 +141,16 @@ fn main() {
             }
 
 
-            if can_be_removed(&entry_path) {
-                println!("{} can be removed", entry_path.display());
-                if ok_to_remove {
-                    remove(&entry_path);
+            match can_be_removed(&entry_path) {
+                Removable::True => {
+                    println!("{} can be removed", entry_path.display());
+                    if ok_to_remove {
+                        remove(&entry_path);
+                    }
                 }
-            } else {
-                println!("must save {}", entry_path.display());
+                Removable::False(why) => {
+                    println!("must save {} (because of {})", entry_path.display(), why.display());
+                }
             }
         } else {
             println!("Warning: Unable to read {:?}", entry.err());
